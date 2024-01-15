9018fb05ecee3bae910dc9f82c26a2ed2accee73
dean ben <19169228+danydin@users.noreply.github.com>
2024-01-15 16:11:41 +0800

    finished ch1 - started ch2

--- ch1_why_http.js
+++ ch1_why_http.js
@@ -1,41 +1,46 @@
 async function main() {
 
   const apiKey = generateKey()
-  const itemURL = 'https://api.boot.dev/v1/courses_rest_api/learn-http/items'
+  const url = getURL()
+  const settings = getSettings()
   
-  const items = await getData(itemURL)
+  const response = await fetch(url, settings)
+  const responseData = await response.json()
   
-  logItems(items)
+  logItems(responseData)
   
   // don't touch below this line
   
-  async function getData(url) {
-    const response = await fetch(url, {
+  function getSettings() {
+    return {
       method: 'GET',
       mode: 'cors',
       headers: {
         'X-API-Key': apiKey,
         'Content-Type': 'application/json'
       }
-    })
-    return response.json()
+    }
+  }
+  
+  function getURL() {
+    return 'https://api.boot.dev/v1/courses_rest_api/learn-http/items'
   }
   
   function generateKey() {
     const characters = 'ABCDEF0123456789'
     let result = ''
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 16; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length))
     }
     return result
   }
   
   function logItems(items) {
-    for (const item of items) {
+    for (item of items) {
       console.log(item.name)
-    } 
+    }
   }
-  
+    
 
   
 }
